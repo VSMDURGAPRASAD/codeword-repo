a40a642f63eab48b8c5f3f3ee233216ce55ef5ea
DurgaPrasadVSM <dp.vinukonda@gmail.com>
2020-01-06 12:46:00 -0800

    Second Part Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InstructorComponent } from './instructor/instructor.component';
+import {StudentComponent } from './student/student.component';
+import {AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import {LoginComponent} from './login/login.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
+const routes: Routes = [
+  {path:'instructor' ,  component: InstructorComponent },
+  {path:'student' ,  component: StudentComponent},
+  {path:'admin' ,  component: AdminDashboardComponent},
+  {path:'login' ,  component: LoginComponent},
+  {path:'' , redirectTo:'instructor',pathMatch:'full'},
+  {path:'**' ,  component:PageNotFoundComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { InstructorComponent } from './instructor/instructor.component';
 import { StudentComponent } from './student/student.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InstructorComponent,
     StudentComponent,
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    LoginComponent,
+    LogoutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
